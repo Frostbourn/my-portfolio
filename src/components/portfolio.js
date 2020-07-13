7e402a99e915799fa24de72f1a49faf72acff09f
Frostbourn <frostbourn@gmail.com>
2020-07-13 18:17:22 +0200

    Remove unnecessary imports

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import Image from "./image"
 
 import { Container, Row, Col } from "react-bootstrap"
 import { Fade, Slide } from "react-awesome-reveal"
