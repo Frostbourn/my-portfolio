1f43e4b8d0fc0947e4877833080a9aeb7fe4f703
Frostbourn <frostbourn@gmail.com>
2020-07-20 14:35:10 +0200

    More particles

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -63,13 +63,11 @@ const Portfolio = () => {
   return (
     <Fade triggerOnce>
       <section id="portfolio" className="portfolio features8">
-      <div className="animation__background">
-        <div className="circle1"></div>
-        <div className="circle2"></div>
-        <div className="circle3"></div>
-        <div className="circle4"></div>
-        <div className="circle5"></div>
-    </div>
+        {/* <div className="animation__background">
+          <div className="big__circle1"></div>
+          <div className="big__circle2"></div>
+          <div className="big__circle3"></div>
+        </div> */}
         <Container>
           <Row className="blog-block justify-content-around">
             <Col md={12}>
