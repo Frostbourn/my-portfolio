11229e2fc44889c778fbf2ce5f18764cddd2b595
Frostbourn <frostbourn@gmail.com>
2020-07-20 10:20:30 +0200

    Changed waves img, dark mode improvements

--- src/components/hero.js
+++ src/components/hero.js
@@ -44,19 +44,15 @@ const Hero = () => (
     <Fade triggerOnce delay={1500}>
       <div className="scroll"></div>
     </Fade>
-
-      <div className="waves content4">
+    <Bounce direction="top" triggerOnce>
+      <div className="waves">
         <figure className="mbr-figure">
           <div className="image-block">
-            <img
-              src="https://jakubskowronski.com/assets/images/bg-waves-img.svg"
-              width="1400"
-              alt="Hero waves"
-              title="Hero waves"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8c43ff" fill-opacity="1" d="M0,192L40,197.3C80,203,160,213,240,186.7C320,160,400,96,480,96C560,96,640,160,720,186.7C800,213,880,203,960,186.7C1040,171,1120,149,1200,144C1280,139,1360,149,1400,154.7L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
           </div>
         </figure>
       </div>
+    </Bounce>
   </section>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,8 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { createGlobalStyle } from "styled-components"
-
-const GlobalStyle = createGlobalStyle`
-   :root {
-    --primary: #293347;
-    --secondary: #8c43ff;
-    --blue: #007bff;
-    --indigo: #6610f2;
-    --purple: #6f42c1;
-    --pink: #e83e8c;
-    --red: #dc3545;
-    }
-    body {
-        font-family: 'Montserrat', sans-serif !important;
-        font-size: 16px;
-        color: var(--primary);
-        overflow-x: hidden;
-    }
-`
-
 const IndexPage = data => (
   <Layout>
-    <GlobalStyle />
     <SEO title="Jakub Skowroński" keywords="frontend developer" />
   </Layout>
 )
