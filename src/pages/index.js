4eea6224b996adf8656b2fe7785de35aa68dd72f
Frostbourn <frostbourn@gmail.com>
2020-07-21 09:44:45 +0200

    Scroll parallax implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { ParallaxProvider } from "react-scroll-parallax"
 
 const IndexPage = data => (
-  <Layout>
-    <SEO title="Jakub Skowroński" keywords="frontend developer" />
-  </Layout>
+  <ParallaxProvider>
+    <Layout>
+      <SEO title="Jakub Skowroński" keywords="frontend developer" />
+    </Layout>
+  </ParallaxProvider>
 )
 
 export default IndexPage
