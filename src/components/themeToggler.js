8712776328bed15b967cdb76a35962c57b58c40b
Frostbourn <frostbourn@gmail.com>
2020-07-27 16:07:08 +0200

    Changed dark mode toggler

--- src/components/themeToggler.js
+++ src/components/themeToggler.js
@@ -1,19 +1,30 @@
-import React, { Fragment } from 'react'
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import React, { Fragment } from "react"
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 const DarkLightSwitch = () => (
-      <ThemeToggler> 
-        {({ theme, toggleTheme }) => (
-          <label className="switch">
-          <input
-            type="checkbox"
-            onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-            checked={theme === 'dark'}
-          />{' '}
-          <span class="slider round"></span>
+  <ThemeToggler>
+    {({ theme, toggleTheme }) => (
+      <div class="toggleWrapper">
+        <input
+          type="checkbox"
+          class="dn"
+          id="dn"
+          onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
+          checked={theme === "dark"}
+        />{" "}
+        <label for="dn" class="toggle">
+          <span class="toggle__handler">
+            <span class="crater crater--1"></span>
+            <span class="crater crater--2"></span>
+            <span class="crater crater--3"></span>
+          </span>
+          <span class="star star--1"></span>
+          <span class="star star--2"></span>
+          <span class="star star--3"></span>
         </label>
-        )}
-      </ThemeToggler>
-    )
+      </div>
+    )}
+  </ThemeToggler>
+)
 
 export default DarkLightSwitch
