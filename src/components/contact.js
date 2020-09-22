98ae9c8e996e7a19584ca89032dd3c7816e658d1
Frostbourn <frostbourn@gmail.com>
2020-09-22 09:55:29 +0200

    components/contact: remove social info

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import { Container, Button, Alert } from "react-bootstrap"
-import { FaFacebookF, FaGithub, FaLinkedin, FaChevronUp } from "react-icons/fa"
+import { FaChevronUp } from "react-icons/fa"
 import { Fade } from "react-awesome-reveal"
 import Emoji from "./Emoji"
 import Background from "../images/map-background.webp"
@@ -61,18 +61,18 @@ const ContactForm = () => {
     <>
       <section
         id="contact"
-        className="contact features8 mbr-section form2 mbr-parallax-background"
+        className="contact features8 mbr-section mbr-parallax-background"
         style={{backgroundImage: `url(${Background})`}}
       >
         <div className="mbr-overlay"></div> 
         <Fade delay="300" triggerOnce>
           <Container>
             <div className="media-container-row">
-              <div className="col-md-6 col-lg-6 block-content">
-                <div className="col-md-10">
+              <div className="col-md-5 col-lg-5 block-content">
+                <div className="col-md-12">
                   <form onSubmit={handleOnSubmit} className="row g-3 d-flex">
                     <h4 className="mb-4 mbr-fonts-style display-2 text-left">
-                      Leave me a message
+                      Leave me a message <Emoji symbol="💬" label="contact" />
                     </h4>
                     {serverState.status && (
                       <Alert
@@ -120,61 +120,6 @@ const ContactForm = () => {
                       Send mail
                     </button>
                   </form>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-6 d-flex justify-content-center">
-                <div className="col-md-10">
-                <div className="text-block">
-                  <h2 className="mb-4 mbr-fonts-style mbr-section-title display-2">
-                    <strong>Contact me <Emoji symbol="💬" label="contact"/></strong>
-                  </h2>
-                  <h3 className="mbr-section-subtitle mbr-fonts-style mb-4 display-5">
-                    You can find me in a few places on the web, but if if you
-                    prefer to do it the "old way" then just write to me message.
-                  </h3>
-                </div>
-                <div className="mb-4 pt-5 mbr-fonts-style subtext display-7">
-                  <div className="social-media col-md-12 col-sm-12">
-                    <ul className="social-list">
-                      <li>
-                        <a
-                          className="icon-transition"
-                          href="https://www.facebook.com/skowronski.jakub"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="mbr-iconfont">
-                            <FaFacebookF />
-                          </span>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="icon-transition"
-                          href="https://github.com/Frostbourn"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="mbr-iconfont">
-                            <FaGithub />
-                          </span>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="icon-transition"
-                          href="https://www.linkedin.com/in/jakubskowronki"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="mbr-iconfont">
-                            <FaLinkedin />
-                          </span>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  </div>
                 </div>
               </div>
             </div>
