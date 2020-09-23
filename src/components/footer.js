b082f164dbff7e43534f86f5bdcba3825b440fe0
Frostbourn <frostbourn@gmail.com>
2020-09-23 15:23:26 +0200

    components/footer: accessibillity improvements

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,45 +15,6 @@ const Footer = () => (
         <div className="col-6 copyright mb-3 mt-3 align-center">
         © {new Date().getFullYear()} {` `}<span className="logo-footer">Jakub<span>Skowronski.</span></span>
         </div>
-        {/* <div className="mb-4 pt-5 mbr-fonts-style subtext display-7">
-          <div className="social-media col-md-12 col-sm-12">
-            <ul className="social-list">
-              <li>
-                <a
-                  className="icon-transition"
-                  href="https://www.facebook.com/skowronski.jakub"
-                  target="_blank"
-                >
-                  <span className="mbr-iconfont">
-                    <FaFacebookF />
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  className="icon-transition"
-                  href="https://github.com/Frostbourn"
-                  target="_blank"
-                >
-                  <span className="mbr-iconfont">
-                    <FaGithub />
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  className="icon-transition"
-                  href="https://www.linkedin.com/in/jakubskowronki"
-                  target="_blank"
-                >
-                  <span className="mbr-iconfont">
-                    <FaLinkedin />
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div> */}
       </div>
     </Container>
   </footer>
