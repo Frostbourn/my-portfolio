8ddb5be5deee908f85bf3ea57f7309330437afc3
Frostbourn <frostbourn@gmail.com>
2020-09-24 08:46:29 +0200

    components/hero: add typing animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,6 +4,7 @@ import { Container, Row, Col } from "react-bootstrap"
 import { Bounce, Fade, Spin } from "react-awesome-reveal"
 import Image from "../images/svgs/hero-image-fourth.svg"
 import Emoji from "./Emoji"
+import Typing from "react-typing-animation"
 
 const Hero = () => {
   const [state, setState] = useState(false)
@@ -37,19 +38,23 @@ const Hero = () => {
         <Container fluid>
           <Row className="align-items-center content-row row hero-margin">
             <Col lg={6} className="hero-text">
-              <Fade direction="left" delay="1000" triggerOnce>
+              <Typing loop>
                 <h1 className="mbr-section-title pb-3 mbr-fonts-style display-1">
                   <strong>Web Developer</strong>
+                  <Typing.Backspace count={15} delay={500} />
+                  <strong>Programmer</strong>
+                  <Typing.Backspace count={15} delay={500} />
+                  <strong>UI/UX Designer</strong>
+                  <Typing.Backspace count={15} delay={500} />
+                  <Typing.Reset count={3} delay={2500} />
                 </h1>
-              </Fade>
-              <Fade direction="left" delay="1400" triggerOnce>
-                <p className="mbr-text pb-3 pl-2 mbr-fonts-style display-5">
-                  I {'\u003Ccode \u002F\u003E'} simple websites and I love it!{" "}
-                  <Emoji symbol="🚀" label="rocket" />
-                </p>
-              </Fade>
+              </Typing>
+              <p className="mbr-text pb-3 pl-2 mbr-fonts-style display-5">
+                I {"\u003Ccode \u002F\u003E"} simple websites and I love it!{" "}
+                <Emoji symbol="🚀" label="rocket" />
+              </p>
               <div className="mbr-section-btn">
-                <Bounce delay="2000" triggerOnce>
+                <Bounce cascade damping="1" triggerOnce>
                   <a
                     className="btn btn-md btn-bgr btn-success display-4"
                     href="https://forms.gle/GJ3GfPGWaQhdpoMR9"
@@ -78,37 +83,35 @@ const Hero = () => {
       <Bounce delay="2300" triggerOnce>
         <div className={`${state ? "hidden" : ""}` + " scroll"}></div>
       </Bounce>
-      <Bounce direction="top" triggerOnce>
-        <div className="waves">
-          <figure className="mbr-figure">
-            <div className="image-block">
-              <svg
-                className="wave-img"
-                style={{ position: "absolute" }}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1440 320"
-              >
-                <path
-                  fill="#8c43ff"
-                  fillOpacity="1"
-                  d="M0,192L40,197.3C80,203,160,213,240,186.7C320,160,400,96,480,96C560,96,640,160,720,186.7C800,213,880,203,960,186.7C1040,171,1120,149,1200,144C1280,139,1360,149,1400,154.7L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-                ></path>
-              </svg>
-              <svg
-                className="wave-img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1440 320"
-              >
-                <path
-                  fill="#8c43ff"
-                  fillOpacity="0.7"
-                  d="M0,32L24,53.3C48,75,96,117,144,149.3C192,181,240,203,288,197.3C336,192,384,160,432,170.7C480,181,528,235,576,240C624,245,672,203,720,160C768,117,816,75,864,74.7C912,75,960,117,1008,138.7C1056,160,1104,160,1152,138.7C1200,117,1248,75,1296,80C1344,85,1392,139,1416,165.3L1440,192L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"
-                ></path>
-              </svg>{" "}
-            </div>
-          </figure>
-        </div>
-      </Bounce>
+      <div className="waves">
+        <figure className="mbr-figure">
+          <div className="image-block">
+            <svg
+              className="wave-img"
+              style={{ position: "absolute" }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+            >
+              <path
+                fill="#8c43ff"
+                fillOpacity="1"
+                d="M0,192L40,197.3C80,203,160,213,240,186.7C320,160,400,96,480,96C560,96,640,160,720,186.7C800,213,880,203,960,186.7C1040,171,1120,149,1200,144C1280,139,1360,149,1400,154.7L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+              ></path>
+            </svg>
+            <svg
+              className="wave-img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+            >
+              <path
+                fill="#8c43ff"
+                fillOpacity="0.7"
+                d="M0,32L24,53.3C48,75,96,117,144,149.3C192,181,240,203,288,197.3C336,192,384,160,432,170.7C480,181,528,235,576,240C624,245,672,203,720,160C768,117,816,75,864,74.7C912,75,960,117,1008,138.7C1056,160,1104,160,1152,138.7C1200,117,1248,75,1296,80C1344,85,1392,139,1416,165.3L1440,192L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"
+              ></path>
+            </svg>{" "}
+          </div>
+        </figure>
+      </div>
     </section>
   )
 }
