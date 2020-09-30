b88b5bf2abeafb8510f19f8f6cf7d886c8c1713c
Frostbourn <frostbourn@gmail.com>
2020-09-30 16:51:43 +0200

    components/footer: rebuild footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,43 @@
 import React from "react"
-import { Container } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 import Emoji from "./Emoji"
+import { Fade } from "react-awesome-reveal"
 
 const Footer = () => (
   <footer>
+    <div className="hidden-height"></div>
     <Container>
-      <div className="media-container-row">
-        <div className="col-md-6 col-lg-6 mb-3 mt-3 align-center">
-          
+      <Row>
+        <nav>
+          <ul class="navbar-nav">
+            <li className="hover">
+              <a href="#about">About</a>
+            </li>
+            <li className="hover">
+              <a href="#portfolio">Work</a>
+            </li>
+            <li className="hover">
+              <a href="#contact">&nbsp;Let's talk!</a>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+      </Row>
+      <Fade delay="3500" triggerOnce>
+        <div className="media-container-row">
+          <div className="col-md-6 col-lg-6 mb-3 mt-3 align-center">
             <div className="mb-0 text-copyright mbr-fonts-style">
-            Made with <Emoji symbol="❤️" label="heart"/> by Jakub Skowronski
+              Made with <Emoji symbol="❤️" label="heart" /> by Jakub Skowronski
+            </div>
+          </div>
+          <div className="col-6 copyright mb-3 mt-3 align-center">
+            ©{new Date().getFullYear()} {` `}
+            <span className="logo-footer">
+              &nbsp;Jakub<span>Skowronski.</span>
+            </span>
           </div>
         </div>
-        <div className="col-6 copyright mb-3 mt-3 align-center">
-        © {new Date().getFullYear()} {` `}<span className="logo-footer">Jakub<span>Skowronski.</span></span>
-        </div>
-      </div>
+      </Fade>
     </Container>
   </footer>
 )
