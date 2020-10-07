e055a7e205051d0a128ed3852ddd71137c3a5cd8
Frostbourn <frostbourn@gmail.com>
2020-10-07 14:54:00 +0200

    components/about: moved bottom wave svg to about section

--- src/components/about.js
+++ src/components/about.js
@@ -22,6 +22,7 @@ const About = () => {
   return (
     <>
       <section id="about" className="about">
+        <div className="about-shapes"></div>
         <Container fluid>
           <Row className="align-items-center content-row">
             {/* <Parallax x={[-5, 5]} styleInner={{"display" : "flex", "align-items" : "center", "justify-content" : "center"}}> */}
@@ -76,6 +77,17 @@ const About = () => {
             {/* </Parallax> */}
           </Row>
         </Container>
+        <svg
+          className="bottom-wave-img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#8c43ff"
+            fillOpacity="1"
+            d="M0,224L60,229.3C120,235,240,245,360,234.7C480,224,600,192,720,176C840,160,960,160,1080,170.7C1200,181,1320,203,1380,213.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+          ></path>
+        </svg>
       </section>
     </>
   )
