bce44638e74fc700d0d6e65a246e016b9cfa315a
Frostbourn <frostbourn@gmail.com>
2020-10-30 16:03:10 +0100

    components/portfolio: remove gatsby-plugin-source

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -56,25 +56,6 @@ const Portfolio = () => {
           }
         }
       }
-      githubViewer {
-        pinnedItems {
-          nodes {
-            name
-            description
-            forkCount
-            stargazers {
-              totalCount
-            }
-            url
-            id
-            diskUsage
-            primaryLanguage {
-              name
-              color
-            }
-          }
-        }
-      }
     }
   `)
   return (
@@ -119,14 +100,15 @@ const Portfolio = () => {
                     width="14"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
                     ></path>
                   </svg>
                   <p className="repo-name">instagram-widget</p>
                 </div>
                 <p className="repo-description">
-                  Simple, lightweight Instagram Widget without API
+                  Simple, lightweight & easy-to-use Instagram Feed plugin for
+                  website
                 </p>
                 <div className="repo-stats">
                   <div className="repo-left-stat">
@@ -148,7 +130,7 @@ const Portfolio = () => {
                         fill="rgb(106, 115, 125)"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
                         ></path>
                       </svg>
@@ -165,7 +147,7 @@ const Portfolio = () => {
                         fill="rgb(106, 115, 125)"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
                         ></path>
                       </svg>
@@ -204,7 +186,7 @@ const Portfolio = () => {
                     width="14"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
                     ></path>
                   </svg>
@@ -235,7 +217,7 @@ const Portfolio = () => {
                         fill="rgb(106, 115, 125)"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
                         ></path>
                       </svg>
@@ -248,100 +230,6 @@ const Portfolio = () => {
                 </div>
               </div>
             </div>
-            {data.githubViewer.pinnedItems.nodes.map((repository, i) => (
-              <div
-                className="card py-3 col-sm-12 col-md-12 col-lg-12"
-                key={repository.id}
-              >
-                <Fade bottom duration={1000} distance="20px" triggerOnce>
-                  <Fade cascade delay="1800" damping="1" triggerOnce>
-                    <div className="card-wrapper">
-                      <div
-                        className="repo-card-div"
-                        onClick={() => openRepoinNewTab(repository.url)}
-                        onKeyDown={() => openRepoinNewTab(repository.url)}
-                        role="presentation"
-                      >
-                        <Badge variant="success">Open Source</Badge>
-                        <div className="repo-name-div">
-                          <svg
-                            aria-hidden="true"
-                            className="octicon"
-                            height="20"
-                            role="img"
-                            viewBox="0 0 12 16"
-                            width="14"
-                            className="repo-svg"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
-                            ></path>
-                          </svg>
-                          <p className="repo-name">{repository.name}</p>
-                        </div>
-                        <p className="repo-description">
-                          {repository.description}
-                        </p>
-                        <div className="repo-stats">
-                          <div className="repo-left-stat">
-                            <span>
-                              <div
-                                className="language-color"
-                                style={{
-                                  backgroundColor:
-                                    repository.primaryLanguage.color,
-                                }}
-                              ></div>
-                              <p>{repository.primaryLanguage.name}</p>
-                            </span>
-                            <span>
-                              <svg
-                                aria-hidden="true"
-                                className="octicon"
-                                height="20"
-                                role="img"
-                                viewBox="0 0 10 16"
-                                width="12"
-                                fill="rgb(106, 115, 125)"
-                                className="repo-star-svg"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
-                                ></path>
-                              </svg>
-                              <p>{repository.forkCount}</p>
-                            </span>
-                            <span>
-                              <svg
-                                aria-hidden="true"
-                                className="octicon"
-                                height="20"
-                                role="img"
-                                viewBox="0 0 14 16"
-                                width="14"
-                                fill="rgb(106, 115, 125)"
-                                className="repo-star-svg"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
-                                ></path>
-                              </svg>
-                              <p>{repository.stargazers.totalCount}</p>
-                            </span>
-                          </div>
-                          <div className="repo-right-stat">
-                            <p>{repository.diskUsage} KB</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Fade>
-                </Fade>
-              </div>
-            ))}
             <div className="card py-3 col-sm-12 col-md-12 col-lg-12">
               <Fade bottom duration={1000} distance="20px" triggerOnce>
                 <div
@@ -397,6 +285,92 @@ const Portfolio = () => {
                   </div>
                 </div>
               </Fade>
+            </div>
+            <div className="card py-3 col-sm-12 col-md-12 col-lg-12">
+              <div
+                className="repo-card-div"
+                onClick={() =>
+                  openRepoinNewTab(
+                    "https://extensions.joomla.org/extension/social-web/social-display/js-like-box-slider/"
+                  )
+                }
+                onKeyDown={() =>
+                  openRepoinNewTab(
+                    "https://extensions.joomla.org/extension/social-web/social-display/js-like-box-slider/"
+                  )
+                }
+                role="presentation"
+              >
+                <span className="badge badge-success">Open Source</span>
+                <div className="repo-name-div">
+                  <svg
+                    aria-hidden="true"
+                    className="repo-svg"
+                    height="20"
+                    role="img"
+                    viewBox="0 0 12 16"
+                    width="14"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
+                    ></path>
+                  </svg>
+                  <p className="repo-name">js-facebook-likebox-slider</p>
+                </div>
+                <p className="repo-description">
+                  Responsive Joomla! plugin that will supercharge your social
+                  sharing & following in a beautiful way.
+                </p>
+                <div className="repo-stats">
+                  <div className="repo-left-stat">
+                    <span>
+                      <div
+                        className="language-color"
+                        style={{ backgroundColor: "#F1E05A" }}
+                      ></div>
+                      <p>PHP</p>
+                    </span>
+                    <span>
+                      <svg
+                        aria-hidden="true"
+                        className="repo-star-svg"
+                        height="20"
+                        role="img"
+                        viewBox="0 0 10 16"
+                        width="12"
+                        fill="rgb(106, 115, 125)"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
+                        ></path>
+                      </svg>
+                      <p>40</p>
+                    </span>
+                    <span>
+                      <svg
+                        aria-hidden="true"
+                        className="repo-star-svg"
+                        height="20"
+                        role="img"
+                        viewBox="0 0 14 16"
+                        width="14"
+                        fill="rgb(106, 115, 125)"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
+                        ></path>
+                      </svg>
+                      <p>1</p>
+                    </span>
+                  </div>
+                  <div className="repo-right-stat">
+                    <p>165 KB</p>
+                  </div>
+                </div>
+              </div>
             </div>
             {/* <div className="card py-3 col-sm-12 col-md-12 col-lg-12">
               <Fade bottom duration={1000} distance="20px" triggerOnce>
