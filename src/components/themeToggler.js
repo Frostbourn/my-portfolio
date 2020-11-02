7f0345a8cec2e3d36183ccbba2e9bf9197b3799f
Frostbourn <frostbourn@gmail.com>
2020-11-02 19:31:29 +0100

    components/themeToggler: fix incorrect initial state being displayed at themeToggler.js

--- src/components/themeToggler.js
+++ src/components/themeToggler.js
@@ -3,29 +3,34 @@ import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 const DarkLightSwitch = () => (
   <ThemeToggler>
-    {({ theme, toggleTheme }) => (
-      <div className="toggleWrapper">
-        <label className="theme-toggler-label" htmlFor="dn">
-          Dark theme toggler
-        </label>
-        <input
-          type="checkbox"
-          className="dn"
-          id="dn"
-          onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-          checked={theme === "dark"}
-        />{" "}
-        <label htmlFor="dn" className="toggle">
-          <span className="toggle__handler">
-            <span className="crater crater--1"></span>
-            <span className="crater crater--2"></span>
-          </span>
-          <span className="star star--1"></span>
-          <span className="star star--2"></span>
-          <span className="star star--3"></span>
-        </label>
-      </div>
-    )}
+    {({ theme, toggleTheme }) => {
+      if (theme == null) {
+        return null
+      }
+      return (
+        <div className="toggleWrapper">
+          <label className="theme-toggler-label" htmlFor="dn">
+            Dark theme toggler
+          </label>
+          <input
+            type="checkbox"
+            className="dn"
+            id="dn"
+            onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
+            checked={theme === "dark"}
+          />{" "}
+          <label htmlFor="dn" className="toggle">
+            <span className="toggle__handler">
+              <span className="crater crater--1"></span>
+              <span className="crater crater--2"></span>
+            </span>
+            <span className="star star--1"></span>
+            <span className="star star--2"></span>
+            <span className="star star--3"></span>
+          </label>
+        </div>
+      )
+    }}
   </ThemeToggler>
 )
 
