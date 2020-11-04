118868cddbd4f805f123f6111894cd2ebdf96dc4
Frostbourn <frostbourn@gmail.com>
2020-11-04 13:30:23 +0100

    gatsby-ssr: add key value to script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,6 +10,7 @@ import React from "react"
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script
+      key="https://socialproof.pl/pixel/yhj3yf4jwiearbesr2b46dsciresnqic"
       src="https://socialproof.pl/pixel/yhj3yf4jwiearbesr2b46dsciresnqic"
       async
     />,
