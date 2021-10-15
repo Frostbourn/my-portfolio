06169c449e5757053de5d5120453070d7c249e58
Frostbourn <frostbourn@gmail.com>
2021-10-15 12:32:19 +0200

    Update env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+<<<<<<< Updated upstream
+=======
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+>>>>>>> Stashed changes
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     {
