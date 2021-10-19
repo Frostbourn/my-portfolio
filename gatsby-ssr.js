15df6116e00fa6e3e6e1c40f0ee52fcfd7c8b0d4
Frostbourn <frostbourn@gmail.com>
2021-10-19 14:15:38 +0200

    Fix portfolio svg margin issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,15 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
 import React from "react"
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script
-      key="pixel"
-      async
+      key="https://socialproof.pl/pixel/yhj3yf4jwiearbesr2b46dsciresnqic"
       src="https://socialproof.pl/pixel/yhj3yf4jwiearbesr2b46dsciresnqic"
-    ></script>,
+      async
+    />,
   ])
 }
